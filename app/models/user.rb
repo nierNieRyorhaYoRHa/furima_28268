25a8325f2c23ea439b61009d9850d4bd240330f3
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-14 12:00:18 +0900

    userモデルの単体テストコードの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ VALID_name_kana_REGEX = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :nickname
     validates :email, format: { with: VALID_email_REGEX }
-    validates :encrypted_password, format: { with: VALID_password_REGEX }
+    validates :password, format: { with: VALID_password_REGEX }
     validates :last_name, format: { with: VALID_name_REGEX }
     validates :first_name, format: { with: VALID_name_REGEX }
     validates :last_name_kana, format: { with: VALID_name_kana_REGEX }
@@ -22,3 +22,8 @@ VALID_name_kana_REGEX = /\A[ァ-ヶー－]+\z/
   end
 
 end
+
+
+
+
+# encrypted_passwordは暗号化されたパスワード、これにバリデーションをかけても意味がない
