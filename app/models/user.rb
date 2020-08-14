48bec13002d8e2f211d35bfc1b581b18d3327f0c
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-14 17:07:17 +0900

    新規登録/ログイン/ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-VALID_email_REGEX = /\A\S+@\S+\.\S+\z/
+VALID_email_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 VALID_password_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i
 VALID_name_REGEX = /\A[ぁ-んァ-ン一-龥]/
 VALID_name_kana_REGEX = /\A[ァ-ヶー－]+\z/
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :users, only: [:new] 
-  resources :items, only: [:new]
+  resources :users
+  resources :items, only: [:index]
 
 end
