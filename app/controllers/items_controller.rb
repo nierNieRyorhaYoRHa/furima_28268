b500f34b877769e9019385a02cf968a0d5e91aaf
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-17 15:23:43 +0900

    item情報の登録実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,12 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+    redirect_to root_path
   end
 
   private
-  def tweet_params
-    params.require(:item).permit(:image, :name, :explanation, :price, :user, :category_id, :status_id, :delivery_id, :area_id, :days_id)
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :delivery_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :area
+  belongs_to_active_hash :days
+
+
+  #空の投稿を保存できないようにする
+  # validates :title, :text, :genre, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  # validates :category_id, numericality: { other_than: 0 } 
+
   belongs_to :user
   has_one :purchase
   has_many :comments
+  has_one_attached :image
 
 end
