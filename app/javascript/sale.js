66f87678160711326899e66113f021e3aa117a7d
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-18 17:45:56 +0900

    非同期的に販売手数料や販売利益を計算

--- app/javascript/sale.js
+++ app/javascript/sale.js
@@ -1,24 +1,15 @@
-function sale() {
-  const price = document.getElementById("item-price");
-  price.addEventListener("oninput", (e) => {
-
-  })
-
-}
-setInterval(check, 1000);
-
-
 $(function(){
-  $("#item_price").on('keyup', function(){
-    var price = $("#item_price").val();
+  $("#item-price").on('keyup', function(){
+    var price = $("#item-price").val();
     if( 300 <= price && price <= 9999999) {
     var fee = Math.floor(price / 10);
     var profit = (price - fee);
-    $(".fee-span").text(fee);
-    $(".profit-span").text(profit);
+    $("span#add-tax-price").text(fee);
+    $("span#profit").text(profit);
     }else{
-    $(".fee-span").text('');
-    $(".profit-span").text('');
+    $("span#add-tax-price").text('');
+    $("span#profit").text('');
     }
   })
 });
+
