d1b533deb6ba5e39f9770036f9ff3f38f077cf88
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-20 15:30:02 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,15 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
