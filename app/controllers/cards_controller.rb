cc579390ba6ff355bd2c58c621838a3a6700b344
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-08-29 16:52:59 +0900

    カード情報の削除機能の実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
-  before_action :card_params, only: [:new, :create]
+  before_action :card_params, only: [:new, :create, :destroy]
+  before_action :set_card
 
   def new
     @user = User.find(current_user.id)
@@ -13,16 +14,30 @@ class CardsController < ApplicationController
     card: params[:card_token] # 登録しようとしているカード情報
     )
 
-    @card = Card.new( # トークン化されたカード情報を保存する
+    card = Card.new( # トークン化されたカード情報を保存する
     card_token: params[:card_token], # カードトークン
     customer_token: customer.id, # 顧客トークン
     user_id: current_user.id # ログインしているユーザー
   )
-    if @card.save
+    if card.save
       redirect_to user_path(current_user)
     else
       render "new" # カード登録画面
     end
+  end
+
+  def destroy
+    @user = User.find(current_user.id)
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    customer = Payjp::Customer.retrieve(@card.customer_token)
+    customer.delete
+
+    if @card.destroy #削除に成功した時にポップアップを表示します。
+      redirect_to action: "new"
+    else #削除に失敗した時にアラートを表示します。
+      redirect_to action: "new"
+    end
+
   end
 
 
@@ -31,6 +46,10 @@ class CardsController < ApplicationController
 
   def card_params
     params.permit(:card_token, :user_id)
+  end
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
 
 end
